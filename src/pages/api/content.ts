76827154e1a9ffad5d4110cda4cf46eced906035
Cher Scarlett <me@cher.dev>
2024-06-22 17:20:55 -0700

    update

--- src/pages/api/content.ts
+++ src/pages/api/content.ts
@@ -1,7 +1,7 @@
 export const home = {
     heading: 'Cher Scarlett',
     paragraphs:  [
-        'I am a student studying physics, mathematics, astronomy, and earth and marine sciences at Orange Coast College. I work at the planetarium teaching the community about earth, space, and aerospace engineering. I practice observational and computational astronomy.',
+        'I am a student studying physics, mathematics, astronomy, and earth science at Orange Coast College. I work at the planetarium teaching the community about earth, space, and aerospace engineering. I practice observational and computational astronomy.',
         'My research interests include solar and extrasolar ocean worlds, palaeogeology, exoplanetary cryospheres, exoplanetary atmospheres, exoplanetary magnetospheres, exomoons, planetary and lunar migration, abiogenisis, atmospheric evolution, atmospheric gravity waves, cloud physics, plasma physics, particle physics, and compact objects.',
         'I taught myself to code in middle school. I\'ve been programming for more than 20 years and have worked in a variety of industries in large corporations such as Apple, Activision Blizzard, Starbucks, and USA Today.',
         'I care deeply for community and involve myself in public policy and community science. I am a passionate advocate of equitable human rights.'
